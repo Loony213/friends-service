9977884d72b4cbf5e4f66c22bf5c7bb8088b5c27
Loony213 <150311939+Loony213@users.noreply.github.com>
2025-07-04 13:09:44 -0500

    delete code

--- get_description/app/routes/description_routes.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from app.controllers.description_controller import get_description
-
-description_bp = Blueprint('description', __name__)
-
-description_bp.route('/get-description', methods=['GET'])(get_description)
--- get_name/app/routes/name_routes.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-from app.controllers.name_controller import get_name
-
-name_bp = Blueprint('name', __name__)
-name_bp.route('/get-name', methods=['GET'])(get_name)
